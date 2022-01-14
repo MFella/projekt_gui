38b6ad27da29021279b21133626eade89b5e3de9
Damian <smugorzewski.ds@gmail.com>
2022-01-14 14:55:19 +0100

    fix

--- src/widgets/Quality/QualityDetails.tsx
+++ src/widgets/Quality/QualityDetails.tsx
@@ -23,49 +23,29 @@ export interface QualityDetailData {
     overallRatio?: OverallRatio | null
 }
 
-//const tagStyle = 
-
+const tagsData = [
+    'Weak',
+    'Average',
+    'Good',
+    'Verygood',
+    'Wonderful'
+];
 
 const QualityDetails = (props: QualityDetailData) => {
     const {t, i18n} = useTranslation();
-    const tagsData = [
-        'Weak',
-        'Average',
-        'Good',
-        'Very good',
-        'Wonderful' 
-    ];
-
     const tagStyle = {minWidth: '4rem', textAlign: 'center', margin: '1rem'} as React.CSSProperties;
-    // should be used in template - doesnt work -> error from mock side XDD
-    const getNextTag = (tagsData: Array<string> = [], tag: any) => {
-        return tagsData[tagsData.findIndex(tag) + 1];
+    const getNextTag = (tagsData: Array<string> = [], tag: string) => {
+        return tagsData[tagsData.findIndex((el) => {return el === tag}) + 1];
     }
 
     return (
         <div className={styles.qualityContainer} >
         <h3>{t("widget.quality.category")}</h3>
             <div className={styles.badgeRateContainer}>
-                {/* {(tagsData.map((tag: any, k: React.Key | null | undefined) => {
-            <Tag round={true} large={true} minimal={true}
-            intent={props.category === tag.title ? Intent.SUCCESS : Intent.NONE} 
-            style={{minWidth: '10rem', textAlign: 'center'}}>{tag.title}</Tag>
-                }))} */}
-                            <Tag round={true} large={false} minimal={true}
-            intent={props.category === 'Weak' ? Intent.SUCCESS : Intent.NONE} 
-            style={tagStyle}>{'Weak'}</Tag>
-                        <Tag round={true} large={false} minimal={true}
-            intent={props.category === 'Average' ? Intent.SUCCESS : Intent.NONE} 
-            style={tagStyle}>{t('Weak')}</Tag>
-                        <Tag round={true} large={false} minimal={true}
-            intent={props.category === 'Good' ? Intent.SUCCESS : Intent.NONE} 
-            style={tagStyle}>{t('Good')}</Tag>
-                        <Tag round={true} large={false} minimal={true}
-            intent={props.category === 'Very good' ? Intent.SUCCESS : Intent.NONE} 
-            style={tagStyle}>{t('Verygood')}</Tag>
-                        <Tag round={true} large={false} minimal={true}
-            intent={props.category === 'Wonderful' ? Intent.SUCCESS : Intent.NONE} 
-            style={tagStyle}>{t('Wonderful')}</Tag>
+                {(tagsData.map((tag: any, k: number) => {
+            return <Tag key={k} round={true} large={true} minimal={true}
+            intent={props.category === tag ? Intent.SUCCESS : Intent.NONE}
+                        style={tagStyle}>{t(tagsData[k])}</Tag>}))}
             </div>
             <div className={styles.aspectsOverallContainer}>
                 <div className={styles.aspectsContainer}>
@@ -97,8 +77,11 @@ const QualityDetails = (props: QualityDetailData) => {
                     <h3>{t("widget.quality.total")}</h3>
                     <h2>{props.overallRatio?.rate}</h2>
                     <p>{t("widget.quality.pointsleft")}<strong>{props.overallRatio?.leftToNext}</strong>{t("widget.quality.left")}</p>
-                    <Tag round={true} large={false} minimal={true}>{props.category}</Tag>
-                    
+                    {props.category &&
+                        <Tag round={true} large={false} minimal={true}>
+                            {t(getNextTag(tagsData, props.category).toString())}
+                        </Tag>
+                    }
                 </div>
             </div>
         </div>
